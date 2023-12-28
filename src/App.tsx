0c0aa5d3bf7f5879b974eca1bad25b67b4711869
Bartosz <bartosz.janiuk@tsh.io>
2023-12-28 17:17:00 +0100

    adjust loading images

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import image from "./react-logo.svg";
+
 export const App = () => {
 	return (
 		<div>
 			<h1>App</h1>
+			<img src={image} alt="React Logo" width={50} height={50} />
 		</div>
 	);
 };
