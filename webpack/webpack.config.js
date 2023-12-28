70e4d1a0436dc892dd9afe48c892507f720be0a1
Bartosz <bartosz.janiuk@tsh.io>
2023-12-28 09:49:56 +0100

    install webpack, babel, react and its dependencies, add webpack base configuration

---
+++ webpack/webpack.config.js
@@ -0,0 +1,56 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+	entry: path.resolve(__dirname, "..", "./src/bootstrap.tsx"),
+	output: {
+		path: path.resolve(__dirname, "..", "./public"),
+		filename: "bundle.js",
+	},
+	mode: "development",
+	resolve: {
+		extensions: [".js", ".ts", ".tsx", ".jsx"],
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: path.resolve(__dirname, "..", "./public/index.html"),
+			inject: true,
+			favicon: "./public/favicon.ico",
+			// ...pluginOptions.htmlWebpackPluginOptions,
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(ts|js)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			//ADD TYPESCRIPT
+			{
+				test: /\.(ts|tsx)$/,
+				exclude: /node_modules/,
+				use: "ts-loader",
+			},
+			// ADD CSS FILES
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				use: ["style-loader", "css-loader"],
+			},
+			// WEBPACK 5 HAS SUPPORT FOR IMAGES OUT OF THE BOX
+			// ADD SUPPORT FOR IMAGES
+			{
+				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+				type: "asset/resource",
+			},
+			// ADD SUPPORT FOR ASSETS
+			{
+				test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+				type: "asset/inline",
+			},
+		],
+	},
+};
