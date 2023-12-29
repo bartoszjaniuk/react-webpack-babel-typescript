6cde2913b9fdc676c20fa22726f7bd83fafd2cb5
Bartosz <bartosz.janiuk@tsh.io>
2023-12-29 09:13:59 +0100

    move plugins and rules into separate files to increase readibility

--- config/utils.ts
+++ config/utils.ts
@@ -1,4 +1,11 @@
 import { NodeEnvironment } from "./setup";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import HTMLWebpackPlugin from "html-webpack-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import path from "path";
+import { pluginOptions } from "./setup";
 
 export const getModes = (mode: NodeEnvironment) => ({
 	isProd: mode === NodeEnvironment.production,
@@ -7,3 +14,76 @@ export const getModes = (mode: NodeEnvironment) => ({
 
 export const useConfig = <T>(condition: boolean, result: T | undefined) =>
 	condition ? result : undefined;
+
+export const useLoader = (
+	condition: boolean,
+	loader: Record<string, unknown> | string,
+) => (condition ? [loader] : []);
+
+export const getWebpackPlugins = (isDev: boolean) => [
+	new BundleAnalyzerPlugin({ analyzerMode: "disabled" }),
+	// new CleanWebpackPlugin(),
+	new HTMLWebpackPlugin({
+		template: path.resolve(__dirname, "..", "./public/index.html"),
+		inject: true,
+		favicon: "./public/favicon.ico",
+		...pluginOptions.htmlWebpackPluginOptions,
+	}),
+	isDev && new ReactRefreshWebpackPlugin(),
+	new MiniCssExtractPlugin({
+		filename: "static/css/[name].[contenthash:8].css",
+		chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
+	}),
+];
+
+export const getModuleRules = (mode: NodeEnvironment) => {
+	const { isProd, isDev } = getModes(mode);
+	return [
+		{
+			test: /\.(ts|js)$/,
+			exclude: /node_modules/,
+			use: {
+				loader: "babel-loader",
+			},
+		},
+		// ADD TYPESCRIPT
+		{
+			test: /\.(ts|tsx)$/,
+			exclude: /node_modules/,
+			use: "ts-loader",
+		},
+		// ADD CSS FILES
+		{
+			test: /\.css$/,
+			exclude: /node_modules/,
+			use: [
+				...useLoader(isDev, { loader: "style-loader" }),
+				...useLoader(isProd, MiniCssExtractPlugin.loader),
+				{
+					loader: "css-loader",
+					options: {
+						importLoaders: 1,
+						sourceMap: !isProd,
+					},
+				},
+				{
+					loader: "postcss-loader",
+					options: {
+						sourceMap: !isProd,
+					},
+				},
+			],
+		},
+		// WEBPACK 5 HAS SUPPORT FOR IMAGES OUT OF THE BOX
+		// ADD SUPPORT FOR IMAGES
+		{
+			test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+			type: "asset/resource",
+		},
+		// ADD SUPPORT FOR ASSETS
+		{
+			test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+			type: "asset/inline",
+		},
+	];
+};
--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -1,79 +1,45 @@
 import path from "path";
-import HTMLWebpackPlugin from "html-webpack-plugin";
-import { Configuration as WebpackConfiguration } from "webpack";
-import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
 
-import { NodeEnvironment, pluginOptions } from "../config/setup";
-import { getModes, useConfig } from "../config/utils";
-import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
 
-interface Configuration extends WebpackConfiguration {
-	devServer?: WebpackDevServerConfiguration;
-}
+import { NodeEnvironment } from "../config/setup";
+import {
+	getModes,
+	getModuleRules,
+	getWebpackPlugins,
+	useConfig,
+} from "../config/utils";
+import { Configuration } from "./interfaces";
 
 export default (mode: NodeEnvironment) => {
 	// TODO: MAKE USE OF IT
 	const { isProd, isDev } = getModes(mode);
 
 	const config: Configuration = {
-		mode,
-		// HOT MODULE REPLACEMENT
+		mode, // performance CHECK THIS
 		devServer: useConfig(isDev, {
+			// HOT MODULE REPLACEMENT
 			hot: true,
 			open: true,
 		}),
-		devtool: isProd ? "cheap-module-source-map" : "source-map",
+		resolve: {
+			extensions: [".js", ".ts", ".tsx", ".jsx"],
+		},
+		devtool: isProd ? "cheap-module-source-map" : "source-map", // it's helpful for debugging code
 		entry: path.resolve(__dirname, "..", "./src/bootstrap.tsx"),
 		output: {
 			path: path.resolve(__dirname, "..", "./public"),
 			filename: "bundle.js",
 		},
-		resolve: {
-			extensions: [".js", ".ts", ".tsx", ".jsx"],
-		},
-		plugins: [
-			new HTMLWebpackPlugin({
-				template: path.resolve(__dirname, "..", "./public/index.html"),
-				inject: true,
-				favicon: "./public/favicon.ico",
-				...pluginOptions.htmlWebpackPluginOptions,
-			}),
-			isDev && new ReactRefreshWebpackPlugin(),
-		],
 		module: {
-			rules: [
-				{
-					test: /\.(ts|js)$/,
-					exclude: /node_modules/,
-					use: {
-						loader: "babel-loader",
-					},
-				},
-				// ADD TYPESCRIPT
-				{
-					test: /\.(ts|tsx)$/,
-					exclude: /node_modules/,
-					use: "ts-loader",
-				},
-				// ADD CSS FILES
-				{
-					test: /\.css$/,
-					exclude: /node_modules/,
-					use: ["style-loader", "css-loader"],
-				},
-				// WEBPACK 5 HAS SUPPORT FOR IMAGES OUT OF THE BOX
-				// ADD SUPPORT FOR IMAGES
-				{
-					test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-					type: "asset/resource",
-				},
-				// ADD SUPPORT FOR ASSETS
-				{
-					test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-					type: "asset/inline",
-				},
-			],
+			rules: [...getModuleRules(mode)],
 		},
+		optimization: {
+			minimize: isProd,
+			// I can omit the TerserPlugin as it's now built into Webpack 5
+			minimizer: [new CssMinimizerPlugin()],
+		},
+		plugins: [...getWebpackPlugins(isDev)],
 	};
 
 	return config;
